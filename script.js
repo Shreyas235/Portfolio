7eb9f55c82b0546047f9c7b06e863bebd68dae64
Shreyas Kowndinya R <skowndinya518@gmail.com>
2024-09-30 12:58:54 +0530

    anims

--- script.js
+++ script.js
@@ -22,6 +22,50 @@ function closemenu() {
   sidemenu.style.right = "-150px";
 }
 
+/* --------------------------------------------------------Change-text----------------------------------------------------------  */
+
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+  let letters = word.textContent.split("");
+  word.textContent = "";
+  letters.forEach((letter) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.className = "letter";
+    word.append(span);
+  });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = () => {
+  let currentWord = words[currentWordIndex];
+  let nextWord =
+    currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+  Array.from(currentWord.children).forEach((letter, i) => {
+    setTimeout(() => {
+      letter.className = "letter out";
+    }, i * 80);
+  });
+  nextWord.style.opacity = "1";
+  Array.from(nextWord.children).forEach((letter, i) => {
+    letter.className = "letter behind";
+    setTimeout(() => {
+      letter.className = "letter in";
+    }, 340 + i * 80);
+  });
+  currentWordIndex =
+    currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+changeText();
+setInterval(changeText, 3000);
+
+/* --------------------------------------------------------For Google Sheets----------------------------------------------------------  */
+
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbyWiuIXlbwVL5JjztvU3dv8Q8QBrnY7JwVTXXuJrrW083JCsuggxkECxRrDvCxOZAH3ZA/exec";
 const form = document.forms["submit-to-google-sheet"];
@@ -34,8 +78,24 @@ form.addEventListener("submit", (e) => {
       msg.innerHTML = "Message sent successfully";
       setTimeout(function () {
         msg.innerHTML = "";
-      }, 3000);
+      }, 5000);
       form.reset();
     })
     .catch((error) => console.error("Error!", error.message));
 });
+
+/* --------------------------------------------------------For toast Notification----------------------------------------------------------  */
+
+let toastBox = document.getElementById("toastBox");
+
+function showToast() {
+  let toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerHTML =
+    '<i class="fa-solid fa-circle-check"></i>Successfully submitted';
+  toastBox.appendChild(toast);
+
+  setTimeout(() => {
+    toast.remove();
+  }, 5000);
+}
